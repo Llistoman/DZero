58dc40a9f921bd29db3f1aed9e8cfe7950e3ca81
UPCman <hamelegarnett@gmail.com>
2016-12-06 19:00:56 +0100

    SUPERMERGE2

---
+++ DZero/Assets/Scripts/UpdateText.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class UpdateText : MonoBehaviour {
+	public Text text;
+	
+	public void UpdateInt(int x)
+	{
+		text.text = x.ToString();
+	}
+	
+	public void UpdateTimer(float x)
+	{
+		text.text = ((int)x/60).ToString("00") + ":" + ((int)x%60).ToString("00") + ":" + ((int)(x*100)%100).ToString("00");
+
+	}
+
+	public void UpdateFloat(float x)
+	{
+		text.text = x.ToString(); 
+	}
+
+	public void UpdateFloat(float x, string y)
+	{
+		text.text = x.ToString(y);
+	}
+	
+	public void UpdateString(string y)
+	{
+		text.text = y;
+	}
+}
